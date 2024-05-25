69875ca1ab0e1f5406fe94698169bac0c4967db5
Divine094 <divinefernandez094@gmail.com>
2024-05-25 18:07:47 +0800

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,7 @@
+const btnLike1 = document.getElementById("btnlike1")
+const countLike1 = document.getElementById("countlike1")
+
+function clicklike1(){
+  let totallikes = parseInt (countLike1.value) + 1
+  countLike1.textContent = totalLikes.toString()
+}
